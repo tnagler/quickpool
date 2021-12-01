88ae9b31a57bb6a49a5130319eed8f71ce285f5c
tnagler <mail@tnagler.com>
2021-12-01 23:38:13 +0100

    single header

--- quickpool.hpp
+++ quickpool.hpp
@@ -18,7 +18,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "aligned_atomic.hpp"
 #include <atomic>
 #include <condition_variable>
 #include <exception>
@@ -37,6 +36,105 @@ static constexpr std::memory_order relaxed = std::memory_order_relaxed;
 static constexpr std::memory_order acquire = std::memory_order_acquire;
 static constexpr std::memory_order release = std::memory_order_release;
 static constexpr std::memory_order seq_cst = std::memory_order_seq_cst;
+
+namespace padding_impl {
+
+// Padding char[]s always must hold at least one char. We do some template
+// metaprogramming to ensure padding is only added if required.
+
+constexpr size_t
+mod(size_t a, size_t b)
+{
+    return a - b * (a / b);
+}
+
+template<class T, size_t Align>
+struct padding_bytes
+{
+    static constexpr size_t obj_size = sizeof(std::atomic<T>);
+    static constexpr size_t free_space = Align - mod(obj_size, Align);
+    static constexpr size_t padding_size_ = free_space > 1 ? free_space : 1;
+    char padding_[padding_size_];
+};
+
+struct empty_struct
+{};
+
+template<class T, size_t Align>
+struct padding
+  : std::conditional<mod(sizeof(std::atomic<T>), Align) != 0,
+                     padding_bytes<T, Align>,
+                     empty_struct>::type
+{};
+
+} // end namespace padding_impl
+
+template<class T, size_t Align = 64>
+struct alignas(Align) aligned_atomic
+  : public std::atomic<T>
+  , private padding_impl::padding<T, Align>
+{
+  public:
+    aligned_atomic() noexcept = default;
+
+    aligned_atomic(T desired) noexcept
+      : std::atomic<T>(desired)
+    {}
+
+    // Assignment operators have been deleted, must redefine.
+    T operator=(T desired) noexcept
+    {
+        this->store(desired);
+        return desired;
+    }
+
+    T operator=(T desired) volatile noexcept
+    {
+        this->store(desired);
+        return desired;
+    }
+
+    // The alloc/dealloc mechanism is pretty much
+    // https://www.boost.org/doc/libs/1_76_0/boost/align/detail/aligned_alloc.hpp
+
+    static void* operator new(size_t count) noexcept
+    {
+        // Make sure alignment is at least that of void*.
+        constexpr size_t alignment =
+          (Align >= alignof(void*)) ? Align : alignof(void*);
+
+        // Allocate enough space required for object and a void*.
+        size_t space = count + alignment + sizeof(void*);
+        void* p = std::malloc(space);
+        if (p == nullptr) {
+            return nullptr;
+        }
+
+        // Shift pointer to leave space for void*.
+        void* p_algn = static_cast<char*>(p) + sizeof(void*);
+        space -= sizeof(void*);
+
+        // Shift pointer further to ensure proper alignment.
+        (void)std::align(alignment, count, p_algn, space);
+
+        // Store unaligned pointer with offset sizeof(void*) before aligned
+        // location. Later we'll know where to look for the pointer telling us
+        // where to free what we malloc()'ed above.
+        *(static_cast<void**>(p_algn) - 1) = p;
+
+        std::cout << "aligned operator" << std::endl;
+
+        return p_algn;
+    }
+
+    static void operator delete(void* ptr)
+    {
+        if (ptr) {
+            std::free(*(static_cast<void**>(ptr) - 1));
+        }
+    }
+};
+
 }
 
 //! @brief Todo list - a synchronization primitive.
@@ -115,7 +213,7 @@ class TodoList
     }
 
   private:
-    aligned_atomic<int> num_tasks_{ 0 };
+    mem::aligned_atomic<int> num_tasks_{ 0 };
     std::mutex mtx_;
     std::condition_variable cv_;
     std::exception_ptr exception_ptr_{ nullptr };
@@ -255,8 +353,8 @@ class TaskQueue
     }
 
   private:
-    aligned_atomic<int> top_{ 0 };
-    aligned_atomic<int> bottom_{ 0 };
+    mem::aligned_atomic<int> top_{ 0 };
+    mem::aligned_atomic<int> bottom_{ 0 };
     std::atomic<RingBuffer<Task*>*> buffer_{ nullptr };
     std::vector<std::unique_ptr<RingBuffer<Task*>>> old_buffers_;
 
@@ -381,8 +479,8 @@ class TaskManager
     const std::thread::id owner_id_;
 
     // task management
-    aligned_atomic<size_t> num_waiting_{ 0 };
-    aligned_atomic<size_t> push_idx_{ 0 };
+    mem::aligned_atomic<size_t> num_waiting_{ 0 };
+    mem::aligned_atomic<size_t> push_idx_{ 0 };
     TodoList todo_list_{ 0 };
     std::exception_ptr err_ptr_{ nullptr };
 

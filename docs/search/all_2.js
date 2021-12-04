01bd167fc27d697bb5ef3ae0cad30f22205a5233
tnagler <thomas.nagler@tum.de>
2021-12-04 19:03:13 +0100

    update docs

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['parallel_5ffor_2',['parallel_for',['../classquickpool_1_1ThreadPool.html#ad734ea599d614c8fb534d2f9e6542fa6',1,'quickpool::ThreadPool::parallel_for()'],['../namespacequickpool.html#aa72b140a64eabe34cd9302bab837c24c',1,'quickpool::parallel_for()']]],
-  ['parallel_5ffor_5feach_3',['parallel_for_each',['../classquickpool_1_1ThreadPool.html#ac11a80ca175424c4c61fd950dda17e0f',1,'quickpool::ThreadPool::parallel_for_each()'],['../namespacequickpool.html#a6655cae9dab967ff085f08c2c91a210c',1,'quickpool::parallel_for_each()']]],
+  ['parallel_5ffor_2',['parallel_for',['../classquickpool_1_1ThreadPool.html#acb9d7efe2053b8d961ed98bcc2cf2618',1,'quickpool::ThreadPool::parallel_for()'],['../namespacequickpool.html#a8c2ad78828bc6ec56f6da75649940889',1,'quickpool::parallel_for()']]],
+  ['parallel_5ffor_5feach_3',['parallel_for_each',['../classquickpool_1_1ThreadPool.html#ad156c576dcd6fb314071ee8a028f2739',1,'quickpool::ThreadPool::parallel_for_each()'],['../namespacequickpool.html#a5542d71824605f77ed621c725d77ed69',1,'quickpool::parallel_for_each()']]],
   ['push_4',['push',['../classquickpool_1_1ThreadPool.html#a3a79d3a078643e271b238ee1be47986e',1,'quickpool::ThreadPool::push()'],['../namespacequickpool.html#a06e138ab2370dc5055a64995ad6c9286',1,'quickpool::push()']]]
 ];

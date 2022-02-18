eb5b856e96407dbb4eede7953339e8e5516fa788
tnagler <mail@tnagler.com>
2022-02-18 14:07:32 +0100

    bump version

--- docs/search/functions_3.js
+++ docs/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['operator_20delete_20',['operator delete',['../classquickpool_1_1ThreadPool.html#a6c1369433f01eb78516e809a98aa1fb6',1,'quickpool::ThreadPool']]],
-  ['operator_20new_21',['operator new',['../classquickpool_1_1ThreadPool.html#a27bc3249d258543c450f0e98492be310',1,'quickpool::ThreadPool']]]
+  ['operator_20new_21',['operator new',['../classquickpool_1_1ThreadPool.html#a283848e5204e82cd93737c7fceabdd1b',1,'quickpool::ThreadPool']]]
 ];
